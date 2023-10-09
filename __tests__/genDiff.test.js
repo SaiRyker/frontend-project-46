924eb15b1471d0ab14857569118dc2ad03a3e844
SaiRyker <daniichernyshov0@gmail.com>
2023-10-09 23:02:42 +0300

    add files for tests and some other files

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/extensions, import/no-extraneous-dependencies
+import { test, expect } from '@jest/globals';
+import * as path from 'node:path';
+// import * as fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+// eslint-disable-next-line import/extensions
+import genDiff from '../src/genDiff.js';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const result = `{
+  - Lunch included: false
+  + Lunch included: true
+  - destination: Los Angeles
+  + destination: New York
+  - plane: Boeing 737
+  + plane: Boeing 738
+    timeOfFlying: 12 hours
+  + Luxury packet: true\n}`;
+
+test('for genDiff', () => {
+  expect(genDiff(getFixturePath('forTest1.json'), getFixturePath('forTest2.json'))).toEqual(result);
+});
