4e3c8eb0878f04b4735722deabaed3467e8e6dda
SaiRyker <daniichernyshov0@gmail.com>
2023-10-14 19:25:30 +0300

    update genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,18 +3,17 @@ import { resolve, extname } from 'node:path';
 import parse  from './parsers.js';
 import searchStatus from './searchStatus.js';
 import formateChoice from './formatters/index.js';
-
+import * as process from 'node:process';
 
 const getExtension = (filepath) => extname(filepath);
-
-const getParsedData = (filepath) => parse(readFileSync(resolve('__fixtures__', filepath)), getExtension(filepath));
+const buildFullPath = (filepath) => resolve(process.cwd(), filepath);
+const getData = (filepath) => parse(readFileSync(filepath, 'utf8'), getExtension(filepath));
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1Data = getParsedData(filepath1);
-  const file2Data = getParsedData(filepath2);
-  
+  const file1Data = getData(buildFullPath(filepath1));
+  const file2Data = getData(buildFullPath(filepath2));
   const dataForFormatting = searchStatus(file1Data, file2Data);
-
+  
   return formateChoice(dataForFormatting, format);
 };
 
